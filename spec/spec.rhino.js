884143d87fb508c22bf876fb86307cf0c5957e33
benbc <ben@bridesmere.com>
2009-12-06 22:23:04 +0000

    First (failing) scenario.

--- spec/spec.rhino.js
+++ spec/spec.rhino.js
@@ -1,9 +1,13 @@
-
-load('gems/gems/jspec-2.11.13/lib/jspec.js')
-load('lib/albumlist.core.js')
+load('gems/gems/jspec-2.11.13/spec/env.js');
+Envjs('spec/fixture.html', { logLevel: Envjs.NONE });
+load('lib/jquery.js');
+load('gems/gems/jspec-2.11.13/lib/jspec.js');
+load('gems/gems/jspec-2.11.13/lib/jspec.jquery.js');
+load('gems/gems/jspec-2.11.13/lib/jspec.xhr.js');
+load('lib/albumlist.core.js');
 
 JSpec
 .exec('spec/spec.core.js')
 .exec('spec/spec.scenarios.js')
 .run({ formatter: JSpec.formatters.Terminal })
-.report()
+.report();
---
+++ spec/spec.scenarios.js
@@ -0,0 +1,29 @@
+JSpec.describe('End-to-end scenarios', function() {
+  it('shows the list of albums', function() {
+    var albums = ['The Tipping Point',
+                  'Black on Both Sides',
+                  'Heartattack and Vine'];
+    server().has(albums);
+    albumlist.application(sandbox());
+    albums.forEach(function(album) {
+      user().shouldSee(album);
+    });
+  });
+});
+
+JSpec.include({ utilities: {
+  server: function() {
+    return {
+      hasAlbums: function() {}
+    };
+  },
+
+  user: function() {
+    var include = matchers.include;
+    return {
+      shouldSee: function(album) {
+        expect(jQuery('.album').text()).to(include, album);
+      }
+    };
+  }
+}});
